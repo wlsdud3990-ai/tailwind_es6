a5d4c75da07d772d9a37c97189ba403831fdd3d1
JINJINJIN <wlsdud3990@gmail.com>
2025-12-18 12:38:14 +0900

    Add files via upload

---
+++ 1217_tailwind_es6/test.js
@@ -0,0 +1,8 @@
+//test.js
+
+let a = 10; //전역변수
+
+function test(){
+  let b = 20; //지역변수
+  return a;
+}
---
+++ 1217_tailwind_es6/test3.js
@@ -0,0 +1,9 @@
+
+//test3.js
+
+//방법1.
+export let num = 'import, export연습하기';
+
+//방법2.
+//let num = 'import, export연습하기';
+//export { num };
